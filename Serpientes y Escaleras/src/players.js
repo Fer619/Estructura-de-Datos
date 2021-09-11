536cb975f7434eed3d3410b3fdadd4bdde2ee349
Fer619 <fbriceno@ucol.mx>
2021-09-10 22:58:49 -0500

    implementation of position

---
+++ Serpientes y Escaleras/src/players.js
@@ -0,0 +1,13 @@
+export default class Players {
+    constructor(position){ 
+    this.position = new Array;
+
+    }
+
+   position() {
+       position= 0;
+       this.position += position.lanzar();
+   }
+    
+    
+}
