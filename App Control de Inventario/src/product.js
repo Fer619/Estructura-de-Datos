e107a2f8d14e0c9fcbcd68a43cb0f1826c3a9054
Fer619 <fbriceno@ucol.mx>
2021-09-20 00:35:48 -0500

    implementation of new Product

--- App Control de Inventario/src/product.js
+++ App Control de Inventario/src/product.js
@@ -23,6 +23,26 @@ export default class Product {
         return this.cost;
     }
 
- 
+    static readForm() {
+        let inpCode = document.querySelector("#code");
+        let inpName = document.querySelector("#name");
+        let inpQuantity = document.querySelector("#quantity");
+        let inpCost = document.querySelector("#cost");
+    
+        let code = Number(inpCode.value);
+        let name = inpName.value;
+        let quantity = Number(inpQuantity.value);
+        let cost = Number(inpCost.value);
+    
+        if (code && name && quantity && cost) {
+          inpCode.value = "";
+          inpName.value = "";
+          inpQuantity.value = "";
+          inpCost.value = "";
+    
+          return new Products(code, name, quantity, cost);
+        }
+        return false;
+      }
 
 }
