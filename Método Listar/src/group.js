238e2d05ea479e4418469f5dd6c8e0636bb3621e
Fer619 <fbriceno@ucol.mx>
2021-10-12 20:52:01 -0500

    implementation of class People and class Group

---
+++ Método Listar/src/group.js
@@ -0,0 +1,19 @@
+
+
+class People {
+    constructor (nombre, id) {
+        this.nombre = nombre;
+        this.id = id;
+        this.next = null;
+    }
+
+    
+}
+class Group {
+    constructor(){
+        this.beginning = null;
+    }
+
+
+
+}
