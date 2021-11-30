eb5f7f1d4b82dab01434ac751fe56c3edabd4953
Fer619 <fbriceno@ucol.mx>
2021-11-30 15:59:21 -0600

    implementation of base y agregar

--- 05Listas Circulares/src/lista.js
+++ 05Listas Circulares/src/lista.js
@@ -1,5 +1,27 @@
 export default class Lista {
     cosntructor (){
-        
+        this.inicio = null;
+        this.length= 0;
     }
+
+    agregar(base){
+        if(this.inicio===null){
+            this.inicio=base;
+            base.next=this.inicio;
+            base.previous=this.inicio;
+            this.length++;
+
+        } else {
+            let last=this.inicio.previous;
+            base.next=this.inicio;
+            base.previous=last;
+            last.next=base;
+            this.inicio.previous=base;
+            this.length++;
+        }
+
+    }
+
+   
+
 }
