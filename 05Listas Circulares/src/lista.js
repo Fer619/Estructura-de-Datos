6d9e9b3f8342ce19ef6e28416a15442f0e566457
Fer619 <fbriceno@ucol.mx>
2021-11-30 16:08:39 -0600

    implementation de método borrar

--- 05Listas Circulares/src/lista.js
+++ 05Listas Circulares/src/lista.js
@@ -22,6 +22,64 @@ export default class Lista {
 
     }
 
+    borrar(base) {
+        let del;
+        let last;
+        let next;
+
+        if(!this.inicio){
+            return null;
+        } else if(this.inicio.getName() === base && this.length===1) {
+            del=this.inicio;
+            this.inicio=null;
+            del.next=null;
+            del.previous=null;
+            this.length--;
+            return del;
+        } else if(this.inicio.getName() === base){
+            del= this.inicio;
+            last= del.previous;
+            next=del.next;
+            this.inicio=next;
+            this.inicio.previous=last;
+            last.next=this.inicio;
+            del.previous=null;
+            del.next=null;
+            this.length--;
+            return del;
+        } else{
+            let prev=this.inicio;
+            let curr=this.inicio.next;
+            while(curr !== this.inicio){
+                if(curr.getName() === base && curr.next === this.inicio){
+                    del=curr;
+                    next=del.next;
+                    prev.next=next;
+                    next.previous=prev;
+                    del.next=null;
+                    del.previous=null;
+                    this.length--;
+                    return del;
+                } else if(curr.getName() === base){
+                    del=curr;
+                    next=del.next;
+                    prev.next=next;
+                    next.previous=prev;
+                    del.next=null;
+                    del.previous=null;
+                    this.length--;
+                    return del;
+                } else{
+                    prev.curr;
+                    curr.curr.next;
+                }
+            }
+
+            return null;
+
+        }
+    }   
+
    
 
 }
